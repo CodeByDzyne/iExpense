19279755d3ffe423978fdeaa2b6417f02f74899e
CodeByDzyne <wdenley@dzynesolutions.com>
2022-10-27 17:59:35 +1100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Wealthy Waz on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
